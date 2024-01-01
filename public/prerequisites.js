6758b01a2cbb590025904e4380b5aebf79d47029
Allan Cueff <allan.cueff@protonmail.com>
2024-01-01 14:29:41 +0100

    redirect if no recap loaded + screen size info

---
+++ public/prerequisites.js
@@ -0,0 +1,14 @@
+if (window.screen.width > 640) {
+    if(window.location.pathname === '/index.html'){
+        document.querySelector('#bad-device').style.display = 'flex';
+        document.querySelector('.presentation').style.display = 'none';
+        document.querySelector('header').style.display = 'none';
+        document.querySelector('body').style.backgroundImage = 'none';
+    } else {
+        window.location = '/index.html';
+    }
+}
+
+if(!('recap' in localStorage) && window.location.pathname !== '/index.html' && window.location.pathname !== '/home.html'){
+    window.location = '/index.html';
+}
